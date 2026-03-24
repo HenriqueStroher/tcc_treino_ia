ad7ecc90886cd26ac64bcbbc5d072089c0ff035b
Henrique Stroher <henriquestroher2003@gmail.com>
2026-03-24 15:36:40 -0300

    Configurando o ambiente 24_03
    Finalizando a configuração inicial do ambiente do projeto

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,7 +17,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
